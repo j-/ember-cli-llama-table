78d3e9343de88a41fb154e7ee2d2dc75a7f37f50
j- <jh@live.com.au>
2015-05-31 17:00:21 +1000

    Ensure jQuery object in scroll handler mixin

--- addon/mixins/scroll-handler.js
+++ addon/mixins/scroll-handler.js
@@ -13,12 +13,12 @@ var ScrollHandlerMixin = Em.Mixin.create({
 	didInsertElement: function () {
 		this._super();
 		var context = this;
-		this.$().on('scroll.scroll-handler-mixin', function (e) {
+		Em.$(this.$()).on('scroll.scroll-handler-mixin', function (e) {
 			context.handleEvent('scroll', e);
 		});
 	},
 	willDestroyElement: function () {
-		this.$().off('scroll.scroll-handler-mixin');
+		Em.$(this.$()).off('scroll.scroll-handler-mixin');
 		this._super();
 	}
 });
